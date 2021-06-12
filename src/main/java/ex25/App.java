58c039af7cdb41519052309f610ad60bac0cbe06
Sebastian <SebastianLSteele@knights.ucf.edu>
2021-06-12 15:00:30 -0400

    created ex25 package

---
+++ src/main/java/ex25/App.java
@@ -0,0 +1,4 @@
+package ex25;
+
+public class App {
+}
---
+++ src/main/java/ex25/AppTest.java
@@ -0,0 +1,27 @@
+
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Sebastian Steele
+ */
+
+package ex25;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    @DisplayName("")
+    public void test__set1(){}
+
+
+}
