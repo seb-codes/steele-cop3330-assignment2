9badc59f90408754c84c0695d76795b3fb47dc4e
Sebastian Steele <SebastianLSteele@knights.ucf.edu>
2021-06-13 17:19:08 -0400

    created ex26 files

---
+++ src/main/java/ex26/AppTest.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Sebastian Steele
+ */
+
+package ex26;
+
+import ex24.App;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    @DisplayName("fill in blank")
+    public void test_anagram_set1(){}
+
+
+}
