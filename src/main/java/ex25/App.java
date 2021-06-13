137a57b31ba152eb9a848ec16610409c14b76d99
Sebastian Steele <SebastianLSteele@knights.ucf.edu>
2021-06-13 17:23:52 -0400

    added headers to files

--- src/main/java/ex25/App.java
+++ src/main/java/ex25/App.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Sebastian Steele
+ */
 package ex25;
 
 import java.util.HashMap;
--- src/main/java/ex26/App.java
+++ src/main/java/ex26/App.java
@@ -1,2 +1,8 @@
-package ex26;public class App {
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Sebastian Steele
+ */
+package ex26;
+
+public class App {
 }
