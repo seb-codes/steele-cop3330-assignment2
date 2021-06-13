05312e9f444bc4be67d8329adae2c6970dfb81b2
Sebastian Steele <SebastianLSteele@knights.ucf.edu>
2021-06-13 17:15:34 -0400

    finished ex25

--- src/main/java/ex25/App.java
+++ src/main/java/ex25/App.java
@@ -1,4 +1,128 @@
 package ex25;
 
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class App {
-}
+    public static boolean numOnly(String val){
+        if(val.matches("[0-9]+"))
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean letOnly(String val){
+        if(val.matches("[a-zA-Z]+"))
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean numletOnly(String val){
+        val = val.toUpperCase();
+        if(val.matches("^(?=.*[A-Z])(?=.*[0-9])[A-Z0-9]+$"))
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean specCheck(String val){
+        Pattern p = Pattern.compile("[^a-zA-Z0-9 ]", Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(val);
+        boolean b = m.find();
+
+        return b;
+    }
+
+    public static boolean lenCheck(String val){
+        int len = val.length();
+        if(len<8)
+            return false;
+        else
+            return true;
+    }
+
+    public static boolean veryWeak(String val){
+        boolean b_len = lenCheck(val);
+        boolean b_num = numOnly(val);
+
+        if(b_len==false && b_num==true)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean Weak(String val){
+        boolean b_len = lenCheck(val);
+        boolean b_let = letOnly(val);
+
+        if(b_len==false && b_let==true)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean Strong(String val){
+        boolean b_len = lenCheck(val);
+        boolean b_numlet = numletOnly(val);
+
+        if(b_numlet==true && b_len==true)
+            return true;
+        else
+            return false;
+    }
+
+    public static boolean veryStrong(String val){
+        boolean b_len = lenCheck(val);
+        boolean b_spec = specCheck(val);
+
+        if(b_spec==true && b_len==true)
+            return true;
+        else
+            return false;
+    }
+
+    public static int passwordValidator(String val){
+        if(veryWeak(val))
+            return 1;
+        if(Weak(val))
+            return 2;
+        if(Strong(val))
+            return 3;
+        if(veryStrong(val))
+            return 4;
+
+        return 0;
+    }
+
+    public static Map<Integer, String> mapBuilder(){
+        Map<Integer, String> m = new HashMap<Integer, String>();
+        m.put(0,"is an invalid password.");
+        m.put(1,"is a very weak password.");
+        m.put(2,"is a weak password.");
+        m.put(3,"is a strong password.");
+        m.put(4,"is a very strong password.");
+
+        return m;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter a password: ");
+        String password = scan.nextLine();
+
+        Map<Integer, String> m = mapBuilder();
+
+        int result = passwordValidator(password);
+
+        System.out.println(String.format("The password '%s' %s",password,m.get(result)));
+
+    }}
--- src/main/java/ex25/AppTest.java
+++ src/main/java/ex25/AppTest.java
@@ -7,6 +7,7 @@
 
 package ex25;
 
+import ex24.App;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,8 +21,50 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    @DisplayName("")
-    public void test__set1(){}
+    @DisplayName("invaled test '%^$#'")
+    public void test__set0(){
+        // simple check, no case differences
+        ex25.App check = new ex25.App();
+        int actual = check.passwordValidator("%^$#");
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("very weak test '12345'")
+    public void test__set1(){
+        // simple check, no case differences
+        ex25.App check = new ex25.App();
+        int actual = check.passwordValidator("12345");
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("weak test 'abcd'")
+    public void test__set2(){
+        // simple check, no case differences
+        ex25.App check = new ex25.App();
+        int actual = check.passwordValidator("abcd");
+        int expected = 2;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("strong test 'abcd123xyz'")
+    public void test__set3(){
+        // simple check, no case differences
+        ex25.App check = new ex25.App();
+        int actual = check.passwordValidator("abcd123xyz");
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("very strong test '1337h@xor!'")
+    public void test__set4(){
+        // simple check, no case differences
+        ex25.App check = new ex25.App();
+        int actual = check.passwordValidator("1337h@xor!");
+        int expected = 4;
+        assertEquals(expected, actual);
+    }
 
 
 }
